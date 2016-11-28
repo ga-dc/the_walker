5eba798c3e8ef655efdd8724751984a7142ab9cd
dfwg4@hotmail.com <dfwg4@hotmail.com>
2016-11-28 17:08:50 -0500

    He moves like my foggy head-all over

--- walker.js
+++ walker.js
@@ -3,32 +3,48 @@ var walker = document.getElementById('walker');
 
 // Configure motion params:
 var walkingLeft = true;
-var leftBorder = 700;
+var leftBorder = 0;
 var rightBorder = /*400;*/window.innerWidth - walker.offsetWidth;
-var speed = 5;
-var xPos = leftBorder;
+var speed = 80;
+var xPos = rightBorder;
 // Have the stick figure start at the right border and start walking left
 // When he crosses the left border, have him turn around and start walking right (and vice versa)
 // Bonus: Make him turn around when you click on him
 function upDate(){
   //xPos += walkingLeft ? -speed : speed;
-  if(walkingRigth){
-    xPos += speed;
+  /*walker.classList.add('flip');
+  xPos += spped;
+  if(xPos >= rightBorder){
+    xPos = leftBorder;
+  }*/
+
+/*walker.style.left = xPos + "px";
+ };*/
+
+if(walkingLeft){
+    xPos -= speed;
   }
   else{
-    xPos -= spped;
+    xPos += speed;
   }
-  walker.style.right = xPos + 'px';
+  walker.style.left = xPos + 'px';
 
-  if (xPos < rightBorder || xPos > leftBorder){
-    walker.classlist.toggle('flip');
-    walkingRight = !walkingRight;
+
+
+if (xPos < leftBorder || xPos > rightBorder)
+    {walker.classList.toggle('flip');
+    walkingLeft = !walkingLeft;
+    //walker.style.left = xPos + 'px';
   }
 }
 
-setInterval (update, 100);
+setInterval (upDate, 100);
 
 walker.addEventListener ('click', function(){
   walker.classList.toggle('flip');
-  walkingRight = !walkingRight;
+  walkingLeft = !walkingLeft;
+});
+
+window.addEventListener("resize", function(){
+  rightBorder = window.innerWidth - walker.offsetWidth;
 });
