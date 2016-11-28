bb0302dc9c84a5533f5b90d2d4e7b650d1de6603
dfwg4@hotmail.com <dfwg4@hotmail.com>
2016-11-28 09:45:39 -0500

    still nothing

--- walker.js
+++ walker.js
@@ -2,27 +2,27 @@
 var walker = document.getElementById('walker');
 
 // Configure motion params:
-var walkingRight = true;
-var leftBorder = 400;
-var rightBorder = 700;//window.innerWidth - walker.offsetWidth;
+var walkingLeft = true;
+var leftBorder = 700;
+var rightBorder = /*400;*/window.innerWidth - walker.offsetWidth;
 var speed = 5;
-var xPos = rightBorder;
+var xPos = leftBorder;
 // Have the stick figure start at the right border and start walking left
 // When he crosses the left border, have him turn around and start walking right (and vice versa)
 // Bonus: Make him turn around when you click on him
 function upDate(){
   //xPos += walkingLeft ? -speed : speed;
-  if(walkingLeft){
-    xPos -= speed;
+  if(walkingRigth){
+    xPos += speed;
   }
   else{
-    xPos += spped;
+    xPos -= spped;
   }
-  walker.style.left = xPos + 'px';
+  walker.style.right = xPos + 'px';
 
-  if (xPos < leftBorder || xPos > rightBorder){
+  if (xPos < rightBorder || xPos > leftBorder){
     walker.classlist.toggle('flip');
-    walkingLeft = !walkingLeft;
+    walkingRight = !walkingRight;
   }
 }
 
@@ -30,5 +30,5 @@ setInterval (update, 100);
 
 walker.addEventListener ('click', function(){
   walker.classList.toggle('flip');
-  walkingLeft = !walkingLeft;
+  walkingRight = !walkingRight;
 });
