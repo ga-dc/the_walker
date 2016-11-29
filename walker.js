c94c3ced1d65d660806358e8b22e29278ac32f0f
“neilburd” <neilburd@gmail.com>
2016-11-29 06:36:26 -0500

    Whatever

--- walker.js
+++ walker.js
@@ -2,7 +2,7 @@
 var walker = document.getElementById('walker');
 
 // Configure motion params:
-var walkingLeft = false;
+var walkingLeft = true;
 var leftBorder = 0;
 var rightBorder = window.innerWidth - walker.offsetWidth;
 var speed = 10;
@@ -13,18 +13,44 @@ var xPos = leftBorder;
 function update() {
   // ensure walker is facing to the right (reverse of "normal")
   // CSS does this for us, we just assign the right class.
-  walker.classList.add("flip");
 
-  // Move "speed" pixels per iteration:
-  xPos += speed;
-  // Reset back to left side, when we run into right wall
-  if(xPos >= rightBorder){
-    xPos = leftBorder;
+  // function walkLeft(){
+  //   xPos -= speed;
+  //   walker.classList.toggle("flip");
+  // }
+  //
+  // function walkRight(){
+  //   xPos += speed;
+  //   walker.classList.toggle("flip");
+  // }
+  // walker.classList.toggle("flip");
+
+  if (walkingLeft){
+    // walkLeft();
+    xPos -= speed;
+  } else {
+    // walkRight();
+    xPos += speed;
   }
-  // reposition the walker
-  walker.style.left = xPos + "px";
-};
 
+  if (walkingLeft){
+    xPos -= speed;
+  }else{
+    xPos += speed;
+  }
+  // Move "speed" pixels per iteration:
+  walker.style.left = xPos + "px";
+
+  if (xPos < leftBorder || xPos > rightBorder) {
+    // css controls visual, we just ensure the correct class is assigned to walker
+    walker.classList.toggle("flip");
+    walkingLeft = !walkingLeft;
+  }
+};
+walker.addEventListener('click', function() {
+  walker.classList.toggle("flip");
+  walkingLeft = !walkingLeft;
+});
 // Establish an update interval (framerate)
 // This will call the "update" method every 100ms
 // Add code to "update" to change the walker's position
